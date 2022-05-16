3ef12ce218a35415c286fe031a1380f844e895ef
Aakash Singh <aakash.singh@unthinkable.co>
2022-05-16 19:59:43 +0530

    commit on 16 may

--- tut.py
+++ tut.py
@@ -360,14 +360,15 @@
 
 
 
-data=[4,5,104,105,110,120,130,140,150,160,170]
+
+# data=[4,5,104,105,110,120,130,140,150,160,170]
 # print(data)
 # del data[0:2]
 # del data[7:]
 # print(data)
-print(data)
-min_value=104
-max_value=150
+# print(data)
+# min_value=104
+# max_value=150
 # for index,value in enumerate(data):
     # if value<min_value or value>max_value: 
         # del data[index]                     # not giving expected output
@@ -379,9 +380,149 @@ max_value=150
 #         del data[index]                     #  giving expected output
 # print(data)
 
-original_last_index=len(data)-1
-for index,value in enumerate(reversed(data)):
-    if value<min_value or value>max_value:
-        del data[original_last_index-index]
-        print(original_last_index-index)
-print(data)
+# original_last_index=len(data)-1
+# for index,value in enumerate(reversed(data)):
+#     if value<min_value or value>max_value:
+#         del data[original_last_index-index]   #length will change so we initialise first
+#         print(original_last_index-index)
+# print(data)
+ 
+# even=[2,4,6,8]
+# odd=[1,3,5,7,9]
+# # number=even+odd
+# number=[even,odd]
+# print(number)
+
+# for number_list in number:
+#     print(number_list)
+
+#     for value in number_list:
+#         print(value)
+
+#nested list
+
+# menu=[
+#     ["egg","bacon"],
+#     ["egg","sausage","bacon"],
+#     ["egg","spam"],
+#     ["egg","bacon","spam"],
+#     ["spam","bacon","sausage","spam"],
+#     ["spam","sausage","spam","bacon","spam","tomato","spam"],
+#     ["spam","egg","spam","spam","bacon","spam"]
+# ]
+
+#1st approach to print menu without spam
+# for meal in menu:
+#     for index in range(len(meal)-1,-1,-1):
+#         if meal[index]=="spam":
+#             del meal[index]
+# print(menu)
+
+#2nd approach to print menu without spam
+
+# for meal in menu:
+#     for item in meal:
+#         if item!="spam":
+#             print(item)
+#     print()
+
+# char_list=["a","b","c","d"]
+# for i in char_list:
+#     print(i,end=" ")
+
+# name="tim"
+# age=20
+# print(name,age,"python",2020,sep=":")
+
+#the function can take several parameters when *object is in definition like print function
+
+#join method
+
+# flowers=[
+#     "Daffodil",
+#     "Evening Primrose",
+#     "Hydrangea",
+#     "Iris",
+#     "Lavender",
+#     "Sunflower",
+#     "Tiger Lily"
+# ]                     # if we add a integer here then join will give error
+# seperator=" | "
+# seperator=","
+# output=seperator.join(flowers)
+# print(output)
+
+#split method
+
+# panagram="The quick brown fox jumps over the lazy dog"
+# panagram="""The quick brown
+# fox jumps\tover
+# the lazy dog"""
+# words=panagram.split() #default splitting argument is space
+# print(words)
+
+# numbers="9,224,453,345,345,675,567"  #to make list from sequence use split for opposite use join
+# print(numbers.split(","))
+# value=numbers.split(",")
+# integer_value=[]
+# for item in value:
+#     integer_value.append(int(item))
+# print(integer_value)
+ 
+#Tuples -  ordered set of data and are immutable
+
+# t="a","b","c"  #can do this but always use parenthesis to avoid some unexpected result
+# t=("a","b","c") 
+# print(t)
+# print(t[0])
+# # t[0]="c" #will give error as tupple is immutable
+# name="Tim"
+# age=10
+# print(name,age,"python",2020)
+# print((name,age,"python",2020))
+
+# a=b=c=d=42
+# print(c)
+# x,y=1,2
+# print(x)
+# print(y)
+
+# data=1,2,3  #data represents tuple
+# x,y,z=data
+# print(x)
+# print(y)
+# print(z)
+
+# #unpacking tuple
+# for t in enumerate("abcdefghij"):   #enumerate return tuples
+#     index,character=t
+#     print(index,character)
+
+#nesting list and tuple
+
+albums=[("welcome to my Nightmare","Alice Cooper",1975),
+         ("Bad Company","Bad company",1974),
+         ("Nightflight","Budgie",1981),
+         ("More Mayhem","Emilda May",2011),
+         ("Ride the Lightning","Metallica",1984),
+         ]
+print(len(albums))
+
+for album_name,artist,year in albums:  #unpacking the tuple
+    print(f"Album: {album_name} ,Artist:{artist} , Year:{year} ")
+
+
+#constants in python
+#By convention constants are defined in all capital letters
+
+#SONGS_LIST=3
+
+#Function in python
+
+def multiply():
+    result=10.5*4
+    return result
+
+answer=multiply()
+print(answer)
+print(multiply())
