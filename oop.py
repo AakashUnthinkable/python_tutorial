e8c5bdc8fcee3fe6b472a1c076ff03907b5f32ed
Aakash Singh <aakash.singh@unthinkable.co>
2022-05-26 18:50:07 +0530

    commit on may 26

--- oop.py
+++ oop.py
@@ -47,34 +47,176 @@
 # print(hamilton.__dict__)
 # print(kenwood.__dict__)
 
- 
-class Account:
-    """Simple account class with balance"""
+# import datetime
+# import pytz
 
-    def __init__(self,name,balance):
+# class Account:
+#     """Simple account class with balance"""
+    
+#     @staticmethod                 #static method
+#     def _current_time():
+#         utc_time=datetime.datetime.utcnow()
+#         return pytz.utc.localize(utc_time)
+
+#     def __init__(self,name,balance):
+#         self.name=name
+#         self.balance=balance
+#         self.transaction_list=[]
+#         print("Account created for "+self.name)
+
+#     def deposit(self,amount):
+#         if amount>0:
+#             self.balance+=amount
+#             self.show_balance()
+#             self.transaction_list.append((Account._current_time,amount))
+#             # self.transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()),amount))
+
+#     def withdraw(self,amount):
+#         if 0<amount<=self.balance:
+#             self.balance-=amount
+#             self.transaction_list.append((Account._current_time(),-amount))
+#         else:
+#             print("amount must be greater than zero and more than account balance")
+
+#     def show_balance(self):
+#         print("Balance is {} ".format(self.balance))
+
+#     def show_transaction(self):
+#         for date,amount in self.transaction_list:
+#             if amount>0:
+#                 tran_type="deposited"
+#             else:
+#                 tran_type="withdrawn"
+#                 amount*=-1
+#             print("{:6} {} on {} )".format(amount,tran_type,date))
+
+# if __name__=='__main__':
+#     tim=Account("Tim",0)
+#     tim.show_balance()
+
+#     tim.deposit(1000)
+#     tim.withdraw(500)
+#     tim.show_balance()
+#     tim.show_transaction()
+
+class Song:
+    """class to represent a song
+    title(str):The title of the song
+    artist(Artist):An Artist object representing the song creator.
+    duration(int):The duration of song in seconds. may be zero
+
+    Attributes:
+    title(str):
+    artist(Artist):
+    duration(int):
+    """
+    def __init__(self,title,artist,duration=0):
+        """
+        song init method
+        Args:
+        title:
+        artist:
+        duration(optional)
+        """
+        self.title=title
+        self.artist=artist
+        self.duration=duration
+
+print(Song.__doc__)  #printing the docstring
+print(Song.__init__.__doc__)
+Song.__init__.__doc__="""updating the docstring"""
+# help(Song)
+#raw string
+
+a_string="this is \na string split\t\t and tabbed"
+print(a_string)
+
+raw_string=r"this  is \n string split \t\t and tabbed\\"
+print(raw_string)
+
+
+class Album:
+    def __init__(self,name,year,artist=None):
         self.name=name
-        self.balance=balance
-        print("Account created for "+self.name)
+        self.year=year
+        if artist is None:
+            self.artist=Artist("various Artists")
+        else:
+            self.artist=artist
+        self.tracks=[]
 
-    def deposit(self,amount):
-        if amount>0:
-            self.balance+=amount
-
-    def withdraw(self,amount):
-        if amount>0:
-            self.balance-=amount
-
-    def show_balance(self):
-        print("Balance is {} ".format(self.balance))
-
+    def add_song(self,song,position=None):
+        if position is None:
+            self.tracks.append(song)
+        else:
+            self.tracks.insert(position,song)
 
 
+# class A:
+#     def test1(self):
+#         print("method named test1 of A called")
 
 
+# class B(A):
+#     def test1(self):
+#         print("method named test1 of B called")
+#         super().test1()
 
 
+# class C(A):
+#     def test1(self):
+#         print("method named test1 of C called")
+#         super().test1()
 
 
+# class D(B,C):
+#     def test2(self):
+#         print("method named test2 of D called") 
 
 
+# object1 = D()
+# object1.test1()
 
+# class A:    
+#     def __init__(self):
+#         print("init of A called") 
+# class B:
+#     def __init__(self):
+#         print("init of B called")
+
+# class C(B,A):
+#     def __init__(self):
+#         super().__init__()
+
+# c = C()
+
+# class Student:
+    
+#     def __init__(self,name):
+#         self._name = name
+        
+# class Student1(Student):
+#     def __init__(self,name):
+#         super().__init__(name)
+
+# s = Student("saif")
+# print(s._name)
+
+# class Base:
+
+#     def __init__(self):
+#         self.multiply(10)
+#         print(self.i)
+
+#     def multiply(self, I):
+#         self.i = 5 * I
+
+# class Derived(Base):
+
+#     def __init__(self):
+#         super().__init__()
+
+#     def multiply(self, I):
+#         self.i = 3 * I
+
+# obj = Derived()
