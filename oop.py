b16ce99b62db367207db739b2fce2bba4526df06
Aakash Singh <aakash.singh@unthinkable.co>
2022-05-30 19:40:32 +0530

    commit on 30 may

--- oop.py
+++ oop.py
@@ -152,6 +152,25 @@
 #             self.tracks.insert(position,song)
 
 
+# class Artist:
+#     """
+#     Basic class to store artist details.
+
+#     Attributes:
+#     name(str):The name of artist
+#     albums: A list of the albums by artist
+
+#     Methods:
+#     add_albums: use to add a new album to artist's albums list
+#     """
+#     def __init__(self,name):
+#         self.name=name
+#         self.albums=[]
+
+#     def add_album(self,album):
+#         self.albums.append(album)
+         
+ 
 # from tkinter import Y
 # from traceback import print_tb
 # from unittest import result
@@ -224,6 +243,38 @@
 #     def multiply(self, I):
 #         self.i = 3 * I
 
-# obj = Derived()
 
+class Player(object):
 
+    def __init__(self,name):
+        self.name=name
+        self._lives=3
+        self.level=1
+        self.score=0
+
+    def _get_lives(self):
+        return self._lives
+
+    def _set_lives(self,lives):
+        if lives>=0:
+            self._lives=lives
+        else:
+            print("lives cannot be negative")
+            self._lives=0 
+
+    lives=property(_get_lives,_set_lives)
+
+    def __str__(self):
+        return ("Name:{0.name}, Lives:{0.lives}, Level:{0.level}, Score {0.score}".format(self))
+
+tim=Player("Tim")
+print(tim.name)
+print(tim.lives)
+tim.lives-=1
+print(tim)
+tim.lives-=1
+print(tim)
+tim.lives-=1
+print(tim)
+tim.lives-=1
+print(tim)
